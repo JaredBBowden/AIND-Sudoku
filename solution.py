784b30f5e396558343f3fd87c530bfd1f53a5fbd
Jared Bowden <jared@Jareds-MacBook-Pro.local>
2017-04-18 22:15:36 -0500

    Adds diagonal solver

--- solution.py
+++ solution.py
@@ -163,7 +163,7 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
-    return search(grid)
+    return search(grid_values(grid))
 
 
 # Define some helper values
@@ -175,7 +175,12 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+
+# Identify units for diagonal solution
+diagonal_units = [[(row + str(i + 1)) for i, row in enumerate(rows)] ,
+                    [(row + str(9 - i)) for i, row in enumerate(rows)]]
+
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
